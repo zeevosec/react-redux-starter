b205a63963ab2f2c017993a42fe9d5fff9df25b7
zeevosec <oneill.shane.h@gmail.com>
2020-04-27 21:22:04 -0400

    feat: update README

--- src/components/GlobalStyles/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
-`;
-
-export default () => <GlobalStyle />;
